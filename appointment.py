d84c770825aa226d33d29dad25252a6e1c3af7af
DK-06 <codecracker86@gmail.com>
2025-10-02 22:56:01 +0530

    completed cancel appointment & print appointments

--- appointment.py
+++ appointment.py
@@ -19,3 +19,25 @@ class Appointment:
         print(f"Appointment booked successfully. Booking Id: {bookingId}\n\n")
 
             
+    def cancelAppointment(self):
+        global appointmentList
+
+        bookingId = str(input("Enter booking Id which want to cancel: "))
+        for appointment in appointmentList:
+            if appointment["id"] == bookingId:
+                appointmentList.remove(appointment)
+                print("Appointment canceled successfully\n\n")
+                return
+
+        print("Booking id not found")
+
+    def getBookedAppointments(self):
+        global appointmentList
+
+        if len(appointmentList) == 0:
+            print("Appointments not booked")
+            return
+
+        for appointment in appointmentList:
+            print(f"Booking ID: {appointment['id']}, Patient name: {appointment['patient_name']}, Reason for visit: {appointment['reason']}\n")
+
